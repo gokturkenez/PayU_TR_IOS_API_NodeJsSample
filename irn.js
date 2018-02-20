62ac7d363a0bd6b6db8be933ce3bf47aaf5805ef
gokturkenez <hi@gokturkenez.com.tr>
2018-02-20 11:31:56 +0300

    Release

---
+++ irn.js
@@ -0,0 +1,34 @@
+/*
+Author: Gokturk Enez
+Mail: hi@gokturkenez.com.tr
+Description: PayU Turkey IOS API Node JS Sample Code
+*/
+
+var EndPointURL = 'https://secure.payu.com.tr/order/ios.php';
+SecretKey = 'SECRET_KEY';
+
+var moment = require('moment');
+date = moment.utc().format('YYYY-MM-DD HH:mm:ss').toString();
+
+var array = {
+    'MERCHANT' : "OPU_TEST",
+    'REFNOEXT' : "7304",
+};
+
+hashstring = '';
+
+for (var k in array) {
+    hashstring += array[k].length + array[k] ;
+}
+var hash = require('crypto')
+    , data = hashstring
+    , secretkey = SecretKey;
+
+signature = hash.createHmac('md5', secretkey).update(data).digest('hex');
+array['HASH'] = signature;
+
+var request = require("request");
+request.post(EndPointURL, {form:array}, function(error, response, body) {
+    console.log(body);
+
+});
